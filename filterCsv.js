0bcaf72892d42b8359e43580563bbb243dda5ee3
Phil Norman <philnorman2@gmail.com>
2019-08-18 17:08:03 -0400

    added Activity_Type to filter

--- filterCsv.js
+++ filterCsv.js
@@ -2,25 +2,25 @@ const csv = require('csv-parser');
 const fs = require('fs');
    
 var stream;
-stream = fs.createWriteStream("./filteredData4.csv");
+stream = fs.createWriteStream("./filteredData5.csv");
 
-console.log(`Date,Time,Distance,Calories,Avg_Pace,Max_HR`);
+console.log(`Activity,Date,Time,Distance,Calories,Avg_Pace,Max_HR`);
 //stream.write(`Date:Time:Distance:Calories:Avg_Pace:Max_HR\n`);
-stream.write(`Date,Time,Distance,Calories,Avg_Pace,Max_HR\n`);
+stream.write(`Activity,Date,Time,Distance,Calories,Avg_Pace,Max_HR\n`);
 
-fs.createReadStream('Activities8.csv')  
+fs.createReadStream('Activities10.csv')  
   .pipe(csv())
   .on('data', (row) => {
     var words = row.Date.split(" ");
     var CaloriesNoCommas = row.Calories.replace(/,/g, '');
-    /*
-    if (row.Avg_Pace.length != 5) {
-       console.log("Avg_Pace length not 5");
-    } 
-    */
-    var Avg_Pace_Pad = row.Avg_Pace.padStart(5, '0');
-    console.log(`"${words[0]}",${words[1]},${row.Distance},${CaloriesNoCommas},"${Avg_Pace_Pad}",${row.Max_HR}\n`);
-    stream.write(`"${words[0]}","${words[1]}","${row.Distance}","${CaloriesNoCommas}","${Avg_Pace_Pad}","${row.Max_HR}"\n`);
+    if (row.Distance != 0) {
+    	var Avg_Pace_Pad = row.Avg_Pace.padStart(5, '0');
+	if (row.Activity_Type == "uncategorized") {
+		row.Activity_Type = 'cycling';
+		}
+    	console.log(`"${row.Activity_Type}","${words[0]}",${words[1]},${row.Distance},${CaloriesNoCommas},"${Avg_Pace_Pad}",${row.Max_HR}\n`);
+    	stream.write(`"${row.Activity_Type}","${words[0]}","${words[1]}","${row.Distance}","${CaloriesNoCommas}","${Avg_Pace_Pad}","${row.Max_HR}"\n`);
+	}
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
