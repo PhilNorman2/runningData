a59493cb3c943752010b1652d14cc69034bbd7fc
Phil Norman <philnorman2@gmail.com>
2019-08-18 16:59:48 -0400

    SQL to create runningStats stored procedure - added activity param

---
+++ runningStatsCreate.sql
@@ -0,0 +1,31 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[runningStats]
+    @startDate DATE,
+    @endDate DATE,
+    @activity NVARCHAR(50)
+AS
+    if (@activity = 'running')
+    BEGIN 
+        SELECT cast(cast(cast(avg(cast(CAST(Avg_Pace as datetime) as float)) as datetime) as time) as varchar(5)) as AvgPace, MAX(Max_HR) as Max_HR, CAST(MIN(Avg_Pace) as NVARCHAR(5)) as MinPace, MAX(Distance) as MaxDistance, ROUND(AVG(Distance),1) as AvgDistance, AVG(Calories) AS AvgCalories 
+        FROM Activities4 where date >= @startDate AND date <= @endDate AND Activity = @activity;
+    END 
+    if (@activity = 'cycling')
+    BEGIN        
+        SELECT ROUND(AVG(cast(RIGHT(cast(Avg_Pace as nvarchar(5)), 4)as float)),1) as Avg_MPH, MAX(Max_HR) as Max_HR, RIGHT(MAX(Avg_Pace), 4) as Max_MPH, MAX(Distance) as MaxDistance, ROUND(AVG(Distance),1) as AvgDistance, AVG(Calories) AS AvgCalories 
+        FROM Activities4 where date >= @startDate AND date <= @endDate AND Activity = @activity;
+    END
+
+
+
+
+
+
+
+
+
+
+
+GO
