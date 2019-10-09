306f82d0d4ada85d37b3f333b8eb486bc79d0f5a
Phil Norman <philnorman2@gmail.com>
2019-10-09 15:11:27 -0400

    Added bike Odometer to the query results

--- runningStatsCreate.sql
+++ runningStatsCreate.sql
@@ -10,13 +10,23 @@ AS
     if (@activity = 'running')
     BEGIN 
         SELECT cast(cast(cast(avg(cast(CAST(Avg_Pace as datetime) as float)) as datetime) as time) as varchar(5)) as AvgPace, MAX(Max_HR) as Max_HR, CAST(MIN(Avg_Pace) as NVARCHAR(5)) as MinPace, MAX(Distance) as MaxDistance, ROUND(AVG(Distance),1) as AvgDistance, AVG(Calories) AS AvgCalories, ROUND(SUM(Distance),0) as SumDistance 
-        FROM Activities4 where date >= @startDate AND date <= @endDate AND Activity = @activity;
+        FROM Activities7 where date >= @startDate AND date <= @endDate AND Activity = @activity;
     END 
     if (@activity = 'cycling')
-    BEGIN        
-        SELECT ROUND(AVG(cast(RIGHT(cast(Avg_Pace as nvarchar(5)), 4)as float)),1) as Avg_MPH, MAX(Max_HR) as Max_HR, RIGHT(MAX(Avg_Pace), 4) as Max_MPH, MAX(Distance) as MaxDistance, ROUND(AVG(Distance),1) as AvgDistance, AVG(Calories) AS AvgCalories, ROUND(SUM(Distance),0) as SumDistance
-        FROM Activities4 where date >= @startDate AND date <= @endDate AND Activity = @activity;
+    BEGIN
+        DECLARE @Odometer FLOAT
+        DECLARE @bikeOdometer FLOAT
+        SET @Odometer = (select odometer from BikeOdometer where device = 'bikeComputer1');
+        SET @bikeOdometer = (select (sum(distance) + @Odometer) from Activities7 WHERE date <= @endDate AND Activity = @Activity)
+        SELECT ROUND(AVG(cast(RIGHT(cast(Avg_Pace as nvarchar(5)), 4)as float)),1) as Avg_MPH, MAX(Max_HR) as Max_HR, RIGHT(MAX(Avg_Pace), 4) as Max_MPH, MAX(Distance) as MaxDistance, ROUND(AVG(Distance),1) as AvgDistance, AVG(Calories) AS AvgCalories, ROUND(SUM(Distance),0) as SumDistance, ROUND(@bikeOdometer,1) AS bikeOdometer
+        FROM Activities7 where date >= @startDate AND date <= @endDate AND Activity = @activity;
     END
+
+
+
+
+
+
 
 
 
