58fbb6c3ab08b4c13b1a42cfa9c10fb328111d27
Phil Norman <philnorman2@gmail.com>
2021-03-05 12:13:07 -0500

    updated pug to ^3.0.2

--- filterCsv.js
+++ filterCsv.js
@@ -2,20 +2,20 @@ const csv = require('csv-parser');
 const fs = require('fs');
    
 var stream;
-stream = fs.createWriteStream("./filteredData5.csv");
+stream = fs.createWriteStream("filteredData13.csv");
 
 console.log(`Activity,Date,Time,Distance,Calories,Avg_Pace,Max_HR`);
-//stream.write(`Date:Time:Distance:Calories:Avg_Pace:Max_HR\n`);
 stream.write(`Activity,Date,Time,Distance,Calories,Avg_Pace,Max_HR\n`);
 
-fs.createReadStream('Activities10.csv')  
+fs.createReadStream('Activities.csv')  
   .pipe(csv())
   .on('data', (row) => {
     var words = row.Date.split(" ");
     var CaloriesNoCommas = row.Calories.replace(/,/g, '');
     if (row.Distance != 0) {
-    	var Avg_Pace_Pad = row.Avg_Pace.padStart(5, '0');
-	if (row.Activity_Type == "uncategorized") {
+      var Avg_Pace_Pad = row.Avg_Pace.padStart(5, '0');
+  //console.log(`row.Activity_Type: ${row.Activity_Type}`);
+	if (row.Activity_Type.toLowerCase() === "uncategorized") {
 		row.Activity_Type = 'cycling';
 		}
     	console.log(`"${row.Activity_Type}","${words[0]}",${words[1]},${row.Distance},${CaloriesNoCommas},"${Avg_Pace_Pad}",${row.Max_HR}\n`);
